2deeb535c4c63ce1759d5ac172b89ccf4ebd42db
vishakhabtcsai26 <1vishakhaa11@gmail.com>
2025-07-28 02:39:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/project22/" : "/", // 👈 Add this line
+
   server: {
     host: "::",
     port: 8080,
